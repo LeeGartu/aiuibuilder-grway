de07235a9abf6024fa3f8dafbcd6465fdc3d620c
ljt <ljt01@gdchico.cn>
2025-12-20 23:27:34 +0800

    修正gitignore

--- package/bsp/src/board.c
+++ package/bsp/src/board.c
@@ -1,11 +1,12 @@
 #include "uart.h"
 #include <rtthread.h>
 
+extern int port_entry(void);
+// static void* __force_ref = (void*)port_entry;  // 强制引用，防止被优化掉
 int board_init(void)
 {
     rt_components_init();
     // uart_init();
-    // port_entry();
     return 0;
 }
 
--- package/bsp/src/port.c
+++ package/bsp/src/port.c
@@ -1,2 +1,9 @@
 #include <rtthread.h>
 
+int port_entry(void)
+{
+    printf("\nhello from %s", __FILE__);
+    return 0;
+}
+
+INIT_COMPONENT_EXPORT(port_entry);
